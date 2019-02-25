720fe4809422fe7b2b7dc309d32215fa04a0c288
SmallPotato <sp.zhangchao@gmail.com>
2019-02-25 16:23:03 +0800

    zc

--- doc.sh
+++ doc.sh
@@ -8,4 +8,5 @@ exit 1
 fi
 
 # 生成 HTML 文档
-pandoc -f markdown -t html5 -c ../assets/github.css -s assets/github.html5 -o doc/$target.html  ./doc-src/$target.md
+# pandoc -f markdown -t html5 -c ../assets/github.css -s assets/github.html5 -o doc/$target.html  ./doc-src/$target.md
+pandoc -f markdown -t html --css assets/pandoc.css -o doc/$target.html  ./doc-src/$target.md
